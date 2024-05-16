17e187e6778d8ecc8acb482e84798d8375d946fc
오민석 <dhalstjr03@naver.com>
2024-05-16 11:00:50 +0900

    전체 서브메뉴 떨어지게 완성

---
+++ 01-GNB/02/js/script.js
@@ -0,0 +1,29 @@
+$(function () {
+  // 대상을 변수에 저장
+  const $header = $('header');
+  const $menu = $('.gnb > li');
+  const $submenu = $('.submenu');
+  const duration = 300; //속도를 변수에 저장
+
+  // 메뉴 영역에 마우스가 들어오면
+  $menu.on('mouseenter', function () {
+    // $(this).find($submenu).stop().slideDown(duration);
+
+    //메뉴를 한번에 떨어뜨리기
+    $submenu.stop().slideDown(duration);
+
+    // 활성화된 메뉴 표시 : on클래스 부여
+    $(this).addClass('on');
+
+    // header에 active 클래스 부여
+    $header.addClass('active');
+  });
+
+  // 메뉴 영역에 마우스가 나가면
+  $menu.on('mouseleave', function () {
+    $submenu.stop().slideUp(duration);
+
+    $menu.removeClass('on');
+    $header.romoveClass('active');
+  });
+});
