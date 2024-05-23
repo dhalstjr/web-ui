9c2d0ed49d8baab1a8ceffc2da350e49a5856974
오민석 <dhalstjr03@naver.com>
2024-05-23 10:48:41 +0900

    기본 동작 완성 -if/else문 사용

--- 04-popup/04-이미지 동영상 갤러리/js/script.js
+++ 04-popup/04-이미지 동영상 갤러리/js/script.js
@@ -4,6 +4,7 @@ $(function () {
   const $dim = $('.dim');
   const $popup = $('.popup');
   const $btnClose = $('.btn-close');
+  const $galleryContent = $('.gallery-content');
   const duration = 300;
 
   // 선택해서 클릭하면 dim이 보이고, 팝업이 뜬다.
@@ -23,6 +24,20 @@ $(function () {
     const videoSrc = $ingEl.data('link');
 
     console.log(imgSrc, imgTitle, videoSrc);
+
+    // 뿌려지는 이미지를 테스트
+    // $galleryContent.text(`<img src="${imgSrc}">`);
+    // $galleryContent.html(`<img src="${imgSrc}">`);
+
+    // !== 다르다(아니라면)
+    // data-link의 값이 있는 상태 --> 블랙핑크 이미지를 클릭했다. --> 동영상을 보여줘야한다.
+    if (videoSrc !== undefined) {
+      // <data-link="${videoSrc}" 가 아니라 iframe src= 들어가야한다.
+      $galleryContent.html(`<iframe src="${videoSrc}">`);
+    } else {
+      // 티셔츠를 이미지를 클릭한 상태
+      $galleryContent.html(`<img src="${imgSrc}">`);
+    }
 
     //
   });
